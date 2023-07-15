7269d3f3f7c0fbdfbb4c8d047a253fa6683656a6
rupeshsahu <rupeshsahu7509@gmail.com>
2023-07-15 17:00:37 +0530

    opening url in chrome

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,24 @@
 package org.example;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+import static com.thoughtworks.selenium.SeleneseTestBase.assertEquals;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-        // Press Ctrl+1 with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","C:\\chromedriver\\chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://rupeshsahu.github.io/");
+        Thread.sleep(7000);
+        driver.close();
+        driver.quit();
 
-        // Press Alt+Shift+X or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Alt+Shift+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+Shift+B.
-            System.out.println("i = " + i);
         }
     }
-}
